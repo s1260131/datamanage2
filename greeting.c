f9d58e49e3275f3ce2afc4992d64ee12160e9823
s1260131 <httanottnk@gmail.com>
2020-08-19 00:18:32 +0900

    User name added

---
+++ greeting.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(void){
+  srand((unsigned int)time(NULL));
+  int n=0;
+  int h=0;
+  int t=0;
+  char str[8];
+  printf("Who are you?\n");
+  scanf("%s",str);
+  printf("Hello,%s!\n",str);
+  printf("Tossing a coin...\n");
+  for(int i=1;i<=3;i++){
+    n=rand()%2;
+    if(n==0){
+      printf("Round %d: Heads\n",i);
+      h++;
+    }else if(n==1){
+      printf("Round %d: Tails\n",i);
+      t++;
+    }
+  }
+  printf("Heads:%d,Tails:%d\n",h,t);
+  return 0;
+}
